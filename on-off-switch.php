e78dc06c823768dc7ec7aa7d8fcb33688d21688e
hrttn <hrettien@gmail.com>
2018-07-27 14:47:28 +0200

    Adding default phpunit

--- on-off-switch.php
+++ on-off-switch.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * WordPress Plugin for an On/Off Switch.
+ * WordPress plugin for an On/Off Switch.
  * 
- * PHP version 5.6
+ * PHP version 7.0
  * 
- * @category   WordPress_Plugin
+ * @category   WordPressPlugin
  * @package    WPElk
  * @subpackage OnOffSwitch
  * @author     WP Elk <hugo@hugorettien.com>
@@ -13,7 +13,7 @@
  * 
  * Plugin Name: On/Off Switch
  * Plugin URI: https://wpelk.com/on-off-switch
- * Description: Display different text whether you are available or not. Use shortcode [on_off_switch] on your page
+ * Description: Display different text whether you are available or not. Use shortcode [on-off-switch] on your page
  * Version: 1.0.0
  * Author: WP Elk
  * Author URI: https://www.wpelk.com/
