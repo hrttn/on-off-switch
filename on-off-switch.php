8438e6b7277323acf9f3df190c8bf5bf8195e026
hrttn <hrettien@gmail.com>
2018-07-27 14:03:54 +0200

    Initial commit

---
+++ on-off-switch.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * WordPress Plugin for an On/Off Switch.
+ * 
+ * PHP version 5.6
+ * 
+ * @category   WordPress_Plugin
+ * @package    WPElk
+ * @subpackage OnOffSwitch
+ * @author     WP Elk <hugo@hugorettien.com>
+ * @license    GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link       https://www.wpelk.com/on-off-switch
+ * 
+ * Plugin Name: On/Off Switch
+ * Plugin URI: https://wpelk.com/on-off-switch
+ * Description: Display different text whether you are available or not. Use shortcode [on_off_switch] on your page
+ * Version: 1.0.0
+ * Author: WP Elk
+ * Author URI: https://www.wpelk.com/
+ * Licence GPLv3
+ * Text Domain: on-off-switch
+ */
+
+namespace WPElk\OnOffSwitch;
